8eafff9eaef644d9c842e41a1528b0da3f6540ff
Sabrina AL HAMMUTI <sabrina@sabrinas-macbook-air.home>
2024-02-24 22:42:56 +0100

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f270c697b762d03d1de9dbd60052b4e4e926e9e6f0beb519b529f1e54d96e5ce
+size 4627
---
+++ pages/layout.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b02fcf8ab7689bc608d843ac89870386cc0b15ba64c53d1ed679e1657216e02a
+size 434
