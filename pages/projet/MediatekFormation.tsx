0dc9b0ee143588ce86bca9a86a6e84d298406f07
Sabrina AL HAMMUTI <sabrina080802@hotmail.fr>
2024-03-08 15:19:52 +0100

    modify

--- pages/projet/MediatekFormation.tsx
+++ pages/projet/MediatekFormation.tsx
@@ -8,6 +8,7 @@ export default function MediatekFormation() {
         <div className="flex flex-col gap-4">
           <h2>MediatekFormation</h2>
           <h4>Application Web • Déc - Fév 2023</h4>
+          <div className="flex gap-2 flex-wrap"></div>
           <Link
             href="https://github.com/sabrina080802/cnedProjet1"
             className="text-purple-600 hover:text-purple-400"
@@ -22,40 +23,40 @@ export default function MediatekFormation() {
             Dossier documentaire
           </Link>
         </div>
-        <p>
-          Pour mon projet de BTS, j'ai développé une application web nommée
-          MediaTek86. Cette plateforme innovante représente un réseau de
-          médiathèques dans la Vienne, offrant un accès à une vaste collection
-          de documents et à divers services numériques via un portail web
-          intuitif. MediaTek86 est dédié à enrichir l'expérience des
-          utilisateurs en intégrant la vidéo à la demande (VOD) et des
-          programmes de formation sur les outils numériques. L'application
-          assure également la gestion technique des médiathèques à travers une
-          équipe spécialisée et le soutien de partenaires stratégiques, visant
-          ainsi à optimiser l'accès et l'utilité des ressources proposées.
-        </p>
-        <p>Ici mettre un relier au cned</p>
-        <div>
-          <h3>Contexte</h3>
-          <Link href="/photo/context.pdf" download className="btn">
-            Télécharger le contexte détaillé{" "}
-          </Link>
-        </div>
-        <div>
-          <h3>Mission</h3>
-          <Link href="/photo/contratDev.pdf" download className="btn">
-            Télécharger le contrat de développement{" "}
-          </Link>
-        </div>
-        <div>
-          <h3>Langage et outils utilisés</h3>
-          <ul>
-            <li>Langages : PHP/TWIG/CSS</li>
-            <li>Serveur : Xampp</li>
-            <li>Framework : Symfony</li>
-            <li>Authentification : Keycloak</li>
-          </ul>
-        </div>
+      </div>
+      <p>
+        Pour mon projet de BTS, j'ai développé une application web nommée
+        MediaTek86. Cette plateforme innovante représente un réseau de
+        médiathèques dans la Vienne, offrant un accès à une vaste collection de
+        documents et à divers services numériques via un portail web intuitif.
+        MediaTek86 est dédié à enrichir l'expérience des utilisateurs en
+        intégrant la vidéo à la demande (VOD) et des programmes de formation sur
+        les outils numériques. L'application assure également la gestion
+        technique des médiathèques à travers une équipe spécialisée et le
+        soutien de partenaires stratégiques, visant ainsi à optimiser l'accès et
+        l'utilité des ressources proposées.
+      </p>
+      <p>Ici mettre un relier au cned</p>
+      <div>
+        <h3>Contexte</h3>
+        <Link href="/photo/context.pdf" download className="btn">
+          Télécharger le contexte détaillé{" "}
+        </Link>
+      </div>
+      <div>
+        <h3>Mission</h3>
+        <Link href="/photo/contratDev.pdf" download className="btn">
+          Télécharger le contrat de développement{" "}
+        </Link>
+      </div>
+      <div>
+        <h3>Langage et outils utilisés</h3>
+        <ul>
+          <li>Langages : PHP/TWIG/CSS</li>
+          <li>Serveur : Xampp</li>
+          <li>Framework : Symfony</li>
+          <li>Authentification : Keycloak</li>
+        </ul>
       </div>
 
       <div className="mt-24 flex flex-col gap-7 justify-start">
@@ -80,7 +81,7 @@ export default function MediatekFormation() {
       </div>
 
       <Image
-        src="/photo/LeJeu.webp"
+        src="/photo/MediatekFormation"
         width={300}
         height={300}
         alt="image"
@@ -89,9 +90,13 @@ export default function MediatekFormation() {
       <div className="mt-24">
         <h3>Ma vidéo de présentation</h3>
         <br />
-        <Link href="" className="btn">
-          Vidéo de présentation{" "}
-        </Link>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/OchD7-gEDRg?si=_9mqAEll_EBRj2Ai"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
       </div>
       <div>
         <Link href="/photo/RapportApp.pdf" download className="btn">
