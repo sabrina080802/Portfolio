342a172faa857457069c509bc09ac5b54be95f5c
Sabrina AL HAMMUTI <sabrina080802@hotmail.fr>
2024-03-11 15:37:35 +0100

    add the contact with email

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,7 +1,6 @@
-import { FormEvent } from "react";
-import { FC } from "react";
-import { useForm } from "react-hook-form";
-import { sendEmail } from "@/app/send-email";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import emailjs from "emailjs-com";
 
 export type FormData = {
   name: string;
@@ -9,12 +8,45 @@ export type FormData = {
   message: string;
 };
 
-const Contact: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const [submitMessage, setSubmitMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>();
 
-  async function onSubmit(data: FormData) {
-    sendEmail(data);
-  }
+  const onSubmit: SubmitHandler<FormData> = async (data, e) => {
+    if (!e) return;
+    emailjs
+      .sendForm(
+        "service_7i4026l",
+        "template_zmhmegw",
+        e.target,
+        "JATCikTfICPZDjEUZ"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSubmitted(true);
+          setSubmitMessage("Message envoyé avec succès !");
+          reset();
+        },
+        (error) => {
+          console.error(error);
+          setSubmitted(true);
+          setSubmitMessage("Échec de l'envoi du message. Veuillez réessayer.");
+        }
+      );
+  };
+
+  // const { register, handleSubmit } = useForm<FormData>();
+
+  // async function onSubmit(data: FormData) {
+  //   sendEmail(data);
+  // }
 
   return (
     <>
@@ -67,6 +99,7 @@ const Contact: FC = () => {
           <button className="btn-type-1">Envoyer</button>
         </div>
       </form>
+      {submitted && submitMessage && <p>{submitMessage}</p>}
     </>
   );
 };
